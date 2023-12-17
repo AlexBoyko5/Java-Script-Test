b8ee8455f5af3b2db168db3ae13c7c0d8982d323
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2023-12-17 13:51:12 +0100

    Update 05 home work.js

--- js/05 home work.js
+++ js/05 home work.js
@@ -4,9 +4,9 @@
 //? Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
 //? У консоль будуть виведені результати її викликів.
 
-const getUserNames = (users) => {
-    return users.map(user => user.name);
-}
+// const getUserNames = (users) => {
+//     return users.map(user => user.name);
+// }
 // return users.map(user => user.name);
 // Цей рядок коду використовує метод (map) для створення нового масиву, де кожен елемент є іменем користувача.
 // (users.map) викликає функцію, передану для обробки кожного елемента масиву (users.)
@@ -24,115 +24,144 @@ const getUserNames = (users) => {
 // };
 
 
-console.log(
-    getUserNames([
-        {
-            name: "Moore Hensley",
-            email: "moorehensley@indexia.com",
-            balance: 2811
-        },
-        {
-            name: "Sharlene Bush",
-            email: "sharlenebush@tubesys.com",
-            balance: 3821
-        },
-        {
-            name: "Ross Vazquez",
-            email: "rossvazquez@xinware.com",
-            balance: 3793
-        },
-        {
-            name: "Elma Head",
-            email: "elmahead@omatom.com",
-            balance: 2278
-        },
-        {
-            name: "Carey Barr",
-            email: "careybarr@nurali.com",
-            balance: 3951
-        },
-        {
-            name: "Blackburn Dotson",
-            email: "blackburndotson@furnigeer.com",
-            balance: 1498
-        },
-        {
-            name: "Sheree Anthony",
-            email: "shereeanthony@kog.com",
-            balance: 2764
-        },
-    ])
-); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+// console.log(
+//     getUserNames([
+//         {
+//             name: "Moore Hensley",
+//             email: "moorehensley@indexia.com",
+//             balance: 2811
+//         },
+//         {
+//             name: "Sharlene Bush",
+//             email: "sharlenebush@tubesys.com",
+//             balance: 3821
+//         },
+//         {
+//             name: "Ross Vazquez",
+//             email: "rossvazquez@xinware.com",
+//             balance: 3793
+//         },
+//         {
+//             name: "Elma Head",
+//             email: "elmahead@omatom.com",
+//             balance: 2278
+//         },
+//         {
+//             name: "Carey Barr",
+//             email: "careybarr@nurali.com",
+//             balance: 3951
+//         },
+//         {
+//             name: "Blackburn Dotson",
+//             email: "blackburndotson@furnigeer.com",
+//             balance: 1498
+//         },
+//         {
+//             name: "Sheree Anthony",
+//             email: "shereeanthony@kog.com",
+//             balance: 2764
+//         },
+//     ])
+// ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
 
 // //!====================== TASK#2 =============================
 // //?Напиши стрілочну функцію getUsersWithFriend(users, friendName) , яка прийматиме два параметра:
 // //?перший параметр users — масив об’єктів користувачів
 // //?другий параметр friendName — ім’я друга для пошуку.
 // //?Функція має повертати масив усіх користувачів із масиву users,
-// //?у яких є друг з іменем friendName.Друзі кожного користувача зберігаються у властивості friends.
+// //?у яких є друг з іменем friendName.
+// //?Друзі кожного користувача зберігаються у властивості friends.
 // //?Якщо користувачів, у яких є такий других немає, то функція має повернути порожній масив.
 // //?  Поради:
 // //?Метод filter() можна використовувати для створення нового масиву з елементами, які задовольняють певну умову.
 // //?Використовуй метод includes() для перевірки, чи масив friends містить friendName.
 // //?Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
-// //?У консоль будуть виведені результати її роботи.
+// //?У консоль будуть виведені результати її роботи.//
 
-// const allUsers = [
-//     {
-//         name: "Moore Hensley",
-//         friends: ["Sharron Pace"]
-//     },
-//     {
-//         name: "Sharlene Bush",
-//         friends: ["Briana Decker", "Sharron Pace"]
-//     },
-//     {
-//         name: "Ross Vazquez",
-//         friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-//     },
-//     {
-//         name: "Elma Head",
-//         friends: ["Goldie Gentry", "Aisha Tran"]
-//     },
-//     {
-//         name: "Carey Barr",
-//         friends: ["Jordan Sampson", "Eddie Strong"]
-//     },
-//     {
-//         name: "Blackburn Dotson",
-//         friends: ["Jacklyn Lucas", "Linda Chapman"]
-//     },
-//     {
-//         name: "Sheree Anthony",
-//         friends: ["Goldie Gentry", "Briana Decker"]
+const getUsersWithFriend = (users, friendName) =>
+    // Використовуємо метод filter() для створення нового масиву users, що містить тільки тих користувачів,
+    // у яких є друг із вказаним ім'ям (friendName)
+    users.filter(user =>
+        // Використовуємо метод includes() для перевірки, чи має поточний користувач друга із вказаним ім'ям
+        user.friends.includes(friendName)
+    );
+
+// const getUsersWithFriend = (users, friendName) =>
+//     users.filter(user => user.friends.includes(friendName));
+
+//? У цьому коді usersFilter використовується для створення нового масиву користувачів(users),
+//?   які задовольняють умову, яку визначає функція, передана як другий аргумент.(friendName)
+//?  У цьому випадку ця функція використовує user.friends.includes для перевірки того,
+//? чи має поточний користувач друга з вказаним ім'ям (friendName).
+
+// При помощи цкла
+// function getUsersWithFriend(users, friendName) {
+//     const filteredUsers = [];
+//     for (let i = 0; i < users.length; i++) {
+//         const user = users[i];
+//         if (user.friends.includes(friendName)) {
+//             filteredUsers.push(user);
+//         }
 //     }
-// ];
+//     return filteredUsers;
+// }
 
-// console.log(getUsersWithFriend(allUsers, "Briana Decker"));
-// // [
-// //   {
-// //     name: "Sharlene Bush",
-// //     friends: ["Briana Decker", "Sharron Pace"]
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"]
-// //   }
-// // ]
+const allUsers = [
+    {
+        name: "Moore Hensley",
+        friends: ["Sharron Pace"]
+    },
+    {
+        name: "Sharlene Bush",
+        friends: ["Briana Decker", "Sharron Pace"]
+    },
+    {
+        name: "Ross Vazquez",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+    },
+    {
+        name: "Elma Head",
+        friends: ["Goldie Gentry", "Aisha Tran"]
+    },
+    {
+        name: "Carey Barr",
+        friends: ["Jordan Sampson", "Eddie Strong"]
+    },
+    {
+        name: "Blackburn Dotson",
+        friends: ["Jacklyn Lucas", "Linda Chapman"]
+    },
+    {
+        name: "Sheree Anthony",
+        friends: ["Goldie Gentry", "Briana Decker"]
+    }
+];
 
-// console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// // [
-// //   {
-// //     name: "Elma Head",
-// //     friends: ["Goldie Gentry", "Aisha Tran"]
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"]
-// //   }
-// // ]
+console.log(getUsersWithFriend(allUsers, "Briana Decker"));
+[
+    {
+        name: "Sharlene Bush",
+        friends: ["Briana Decker", "Sharron Pace"]
+    },
+    {
+        name: "Sheree Anthony",
+        friends: ["Goldie Gentry", "Briana Decker"]
+    }
+]
 
-// console.log(getUsersWithFriend(allUsers, "Adrian Cross")); // []
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+[
+    {
+        name: "Elma Head",
+        friends: ["Goldie Gentry", "Aisha Tran"]
+    },
+    {
+        name: "Sheree Anthony",
+        friends: ["Goldie Gentry", "Briana Decker"]
+    }
+]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross")); // []
 
 // //!====================== TASK#3 =============================
 // //? Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
