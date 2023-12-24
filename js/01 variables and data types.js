6f5713d0484a7e41ae0bf44715d618a8f9c6c053
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2023-12-24 16:57:46 +0100

    upd-block1 #1

---
+++ js/01 variables and data types.js
@@ -0,0 +1,29 @@
+// let distance = 617.54
+// const login = "mango935"
+// const isOnline = true
+// console.log(typeof distance);
+// console.log(typeof login);
+// console.log(typeof isOnline);
+//^ ===================================================================================================================
+// const x = 8;
+// const y = 5;
+// console.log(x % y);
+//^ =====================================================================================================================================
+//? Магазин з продажу ремонтних дроїдів готовий до відкриття, залишилося написати скрипт для їх замовлення.Оголоси змінні та присвой їм відповідні значення:
+//? pricePerDroid - ціна одного дроїда, значення 800
+//? orderedQuantity - кількість дроїдів у замовленні, значення 6
+//? deliveryFee - вартість доставки, значення 50
+//? totalPrice - загальна сума замовлення до сплати(ціна одного дроїда помножена на кількість дроїдів у замовленні, плюс вартість доставки)
+//? message - повідомлення про стан замовлення у форматі "You ordered droids worth <total price> credits"
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits`
+//^ ===================================================================================================================
+const username = "Poly";
+// const username = "Harambe";
+// const username = "Billy";
+// const username = "Joe";
+const message = `Username ${username} is ${username.length} characters long`
+console.log(message);
