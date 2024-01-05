ecf224960e48c381baebd18e032c33bc30556a0b
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-01-05 17:32:38 +0100

    commit-array#2

--- js/100 home work.js
+++ js/100 home work.js
@@ -240,16 +240,50 @@
 // функція повинна повернути копію масиву з довжиною maxLength елементів.
 // В іншому випадку функція повинна повернути весь новий масив.
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+// function makeArray(firstArray, secondArray, maxLength) { //* Оголошуємо функцію*/
+//   const newAray = firstArray.concat(secondArray);
+//   if (newAray.length > maxLength) {
+//     return newAray.slice(0, maxLength); //*  повертаємо копію масиву з довжиною (maxLength)
+//   } else {
+//     return newAray; //* повертаємо весь новий масив*/
+//   }
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
 //!====================== TASK#3 =============================
 
-//~ ========================  Д/З======================== ======================== ======================== 
+// Напиши функцію filterArray(numbers, value), яка приймає масив чисел(numbers)
+// та значення(value) як параметри.
+// Функція повинна повертати новий масив лише тих чисел
+// із масиву numbers, які більші за значення value.
+// Усередині функції:
+// Створи порожній масив, у який будеш додавати підходящі числа.
+// Використай цикл для ітерації кожного елемента масиву numbers.
+// Використовуй умовний оператор if усередині циклу для перевірки
+//  кожного елемента и додавання до свого масиву.
+// Поверни свій новий масив з підходящими числами як результат.
+
+function filterArray(numbers, value) {
+  const newArray = []
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+  }
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+//~ ========================  Д/З ======================================================================== 
 //!====================== TASK#1 =============================
 //? Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об’єктів користувачів.
 //? Функція має повертати масив імен усіх користувачів(властивість name) із масиву users.
